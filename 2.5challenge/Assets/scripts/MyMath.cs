18c269e246059e6298110f310fa51eb4ae19c28f
EbFenzlaff <111587038+EbFenzlaff@users.noreply.github.com>
2022-10-25 10:38:09 -0600

    working on method coding

---
+++ 2.5challenge/Assets/scripts/MyMath.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyMath : MonoBehaviour
+{
+    int num = 1200;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        product();
+        Debug.Log(num);
+
+    }
+
+    void product()
+
+    {
+        num = num * 2;
+   
+    }
+}
